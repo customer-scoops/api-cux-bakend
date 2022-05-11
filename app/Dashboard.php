54c660de341e422c09e8bcd82054e2f2e7e79e84
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-05-11 11:34:30 -0300

    Fixes Tranking

--- app/Dashboard.php
+++ app/Dashboard.php
@@ -6182,7 +6182,7 @@ class Dashboard extends Generic
             $detailGeneration   = substr($request->survey, 3, 3) == 'via' ? $this->ranking($db, 'convenio', 'Convenio', $endDateFilterMonth, $startDateFilterMonth, $filterClient,$datafilters, 6, 5) : null;
             $detailsProcedencia = substr($request->survey, 3, 3) == 'via' ? $this->graphINS($tiempoVehiculo, $coordAnden, $tiempoAeropuerto, $tiempoLlegadaAnden) : null;
             $box14              = substr($request->survey, 3, 3) == 'con' ? $this->CSATDrivers($graphCSATDrivers) : $this->graphCsatTransvip($drivers, $request->survey);
-            $box15              = null; //substr($request->survey, 3, 3) == 'via' ? $this->traking($db, $startDateFilterMonth, $endDateFilterMonth) : null;
+            $box15              = substr($request->survey, 3, 3) == 'via' ? $this->traking($db, $startDateFilterMonth, $endDateFilterMonth) : null;
             $box16              = substr($request->survey, 3, 3) == 'con' ? $proveedor : null;
             $box17              = substr($request->survey, 3, 3) == 'con' ? $frecCon : null;
             $box18              = substr($request->survey, 3, 3) == 'con' ? $contactoEmpresas : null;
