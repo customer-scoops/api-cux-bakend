a1810d3398035832eb2271b3dcf393fd726c04c0
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-05-16 16:05:24 -0300

    fix ban

--- app/Dashboard.php
+++ app/Dashboard.php
@@ -2996,7 +2996,7 @@ class Dashboard extends Generic
                                 ((count(if($indicador < 7, $indicador, NULL))*100)/COUNT(CASE WHEN $indicador!=99 THEN 1 END))*$this->_porcentageBan as detractor, 
                                 ((count(if($indicador> 8 AND $indicador <=10, $indicador, NULL))*100)/COUNT(CASE WHEN $indicador!=99 THEN 1 END))*$this->_porcentageBan as promotor, 
                                 ((count(if($indicador=8 OR $indicador=7, $indicador, NULL))*100)/COUNT(CASE WHEN $indicador!=99 THEN 1 END))*$this->_porcentageBan as neutral,
-                                a.mes, a.annio, date_survey, SUBDATE(date_survey, WEEKDAY(date_survey)) as mondayWeek, $this->_fieldSelectInQuery 
+                                a.mes, a.annio, date_survey, $this->_fieldSelectInQuery 
                                 FROM $this->_dbSelected.$table as a
                                 INNER JOIN $this->_dbSelected." . $table . "_start as b on a.token = b.token
                                 WHERE date_survey BETWEEN '$dateEnd' AND '$dateIni'  $datafilters	
